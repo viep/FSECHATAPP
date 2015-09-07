19113eda7c58afee186d6b36cbef6f8257df003c
viep <vinoep@gmail.com>
2015-09-07 14:13:38 -0700

    First commit

--- index.js
+++ index.js
@@ -15,11 +15,12 @@ res.sendFile('index.html', { root: __dirname });
 app.get('/log_out.html', function(req, res){
 res.sendFile('log_out.html', { root: __dirname });
 });
+
 // Session starts
 io.on('connection', function(socket){
   socket.on('join',function(user){
   socket.username=user;
- socket.emit('welcome message', user + " Welcome to the Chat Room");
+ socket.emit('welcome message', socket.username + ", Welcome to the Chat Room");
 
   db.serialize(function() {
   db.run("CREATE TABLE if not exists user (name TEXT, login TEXT)");
@@ -30,7 +31,8 @@ io.on('connection', function(socket){
   stmt.run(socket.username, n);
   stmt.finalize();
 
-/* 
+/*  To clean up db
+ 
   db.run(" DROP TABLE if exists user;",function(err){
      console.log("Error is "+ err);
 });
@@ -40,22 +42,19 @@ io.on('connection', function(socket){
  });
 */
 
+// showing history
   db.each("SELECT * FROM(SELECT * FROM msglog ORDER BY time DESC limit 10) tmp ORDER by tmp.time ASC;", function(err, row) {
-  //    console.log("User Name : "+row.name + "Message ::" + row.msg + "Time Stamps::"+ row.time);
      socket.emit('chat message', '<div id="user">' + row.name+'</div><div id="mesg">'+row.msg+'</div><div id="times">'+row.time +'</div>');
-   // socket.emit ('history',row.time);
-     //socket.emit ('history',row.msg);
   });
 
 });
 });
 
+// for handling real time chatting
   socket.on('chat message', function(msg){
    var d = new Date();
   var n = d.toLocaleTimeString();
    io.emit('chat message', '<div id="user">' + socket.username+'</div><div id="mesg">'+msg+'</div><div id="times">'+n +'</div>');
-  // io.emit('chat message', n);
-  // io.emit('chat message', msg);
   var stmt = db.prepare("INSERT INTO msglog VALUES (?,?,?)");
   stmt.run(socket.username,msg, utcDate);
   stmt.finalize();
